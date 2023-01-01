782767fc7c6cf6262fe5c2ae379baf84b9f13308
dystopianctzn <kinda.toxic11@gmail.com>
2023-01-02 00:57:11 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Mainclass
+{
+    public static void Main(string[] args)
+    {
+        string MyName = "Дмитрий";
+        int MyAge = 21;
+        bool HavePet = true;
+        double ShoeSize = 41.5;
+
+        Console.WriteLine("Меня зовут "  + MyName);
+        Console.WriteLine("Мне " + MyAge);
+        Console.WriteLine("Есть ли у меня питомец? " + HavePet);
+        Console.WriteLine(" Мой размер обуви " + ShoeSize);
+
+    }
+}
